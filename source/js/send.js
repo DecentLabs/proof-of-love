d0818bea9251d28515c4ec49fdc3c96f76ab4cc9
robi <robert.szaloki@euedge.com>
2018-10-08 15:38:09 +0200

    proof html

--- source/js/send.js
+++ source/js/send.js
@@ -4,6 +4,7 @@ window.addEventListener('load', function() {
   if (typeof web3 !== 'undefined') {
     // Use Mist/MetaMask's provider
     web3js = new Web3(web3.currentProvider);
+    //TODO: send names to eth
   } else {
     console.log('No web3? You should consider trying MetaMask!')
     window.location.href='/no-metamask.html'
