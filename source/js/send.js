45ada4dbab003f438e52656d5de9ef5ed489d872
robi <robert.szaloki@euedge.com>
2018-10-08 15:16:55 +0200

    with heart!

---
+++ source/js/send.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', function() {
+
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+    // Use Mist/MetaMask's provider
+    web3js = new Web3(web3.currentProvider);
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+    window.location.href='/no-metamask.html'
+  }
+})
