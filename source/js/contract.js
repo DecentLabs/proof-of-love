5b61e8287db176957cbf433fc829317404fcff60
Daniel Fogaras <daniel.fogaras@gmail.com>
2018-10-09 13:33:52 +0200

    format abi

--- source/js/contract.js
+++ source/js/contract.js
@@ -1,4 +1,66 @@
-export const ABI = [{"constant":true,"inputs":[],"name":"count","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"name1","type":"string"},{"name":"name2","type":"string"}],"name":"prove","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"name1","type":"string"},{"indexed":false,"name":"name2","type":"string"}],"name":"Love","type":"event"}]
+export const ABI = [
+   {
+      'constant':true,
+      'inputs':[
+
+      ],
+      'name':'count',
+      'outputs':[
+         {
+            'name':'',
+            'type':'uint256'
+         }
+      ],
+      'payable':false,
+      'stateMutability':'view',
+      'type':'function'
+   },
+   {
+      'constant':false,
+      'inputs':[
+         {
+            'name':'name1',
+            'type':'string'
+         },
+         {
+            'name':'name2',
+            'type':'string'
+         }
+      ],
+      'name':'prove',
+      'outputs':[
+
+      ],
+      'payable':false,
+      'stateMutability':'nonpayable',
+      'type':'function'
+   },
+   {
+      'inputs':[
+
+      ],
+      'payable':false,
+      'stateMutability':'nonpayable',
+      'type':'constructor'
+   },
+   {
+      'anonymous':false,
+      'inputs':[
+         {
+            'indexed':false,
+            'name':'name1',
+            'type':'string'
+         },
+         {
+            'indexed':false,
+            'name':'name2',
+            'type':'string'
+         }
+      ],
+      'name':'Love',
+      'type':'event'
+   }
+]
 
 export const ADDRESS = '0x5C43a1e3D4e48De3D568Dcf2995d61f58330E4C2'
 //'0x00CF09F905b0485e1a6b7845693DBd63c6922986'
