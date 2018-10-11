b86ba0d7b34e2e19d5e57744d2349e0dc709a8f1
robi <robert.szaloki@euedge.com>
2018-10-11 14:30:51 +0200

    html proof page

--- source/js/partials.js
+++ source/js/partials.js
@@ -1,16 +1,13 @@
 import {html, render} from 'https://unpkg.com/lit-html?module';
 
-const pending = html`<h1>Prooving love</h1>`
+const pending = html`<h1>Proooooving love</h1>`
 
 const confirmed = (state) => html`
-<p>The love between <b>${state.names && state.names[0]}</b> and <b>${state.names && state.names[1]}</b>
-  has been proven by carving into the blockchain</p>
-<p>The transaction ID is ${state.transactionID}</p>
 <canvas id="heart-canvas"></canvas>
-<div>confirmed from: ${state.confirmed}</div>
+<p>Carved forever in the Blockchain since ${state.timestamp} at<br><span class="tx">${state.transactionID}</span></p>
 `
 
-const main = (state) => html`<div>${state.pending ? pending : confirmed(state)}</div>`
+const main = (state) => html`${state.pending ? pending : confirmed(state)}`
 
 export function renderMain(state, element) {
   render(main(state), element)
