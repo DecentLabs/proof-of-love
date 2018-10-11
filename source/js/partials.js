50ada2cb1dc0ab9fd10999f440555ea7ece8c933
juli_e <julia.ero@euedge.com>
2018-10-11 14:57:29 +0200

    download heart

--- source/js/partials.js
+++ source/js/partials.js
@@ -3,8 +3,13 @@ import {html, render} from 'https://unpkg.com/lit-html?module';
 const pending = html`<h1>Proooooving love</h1>`
 
 const confirmed = (state) => html`
+<div class="row">
+<h2>${state.names && state.names[0]}</h2>
 <canvas id="heart-canvas"></canvas>
+<h2>${state.names && state.names[1]}</h2>
+</div>
 <p>Carved forever in the Blockchain since ${state.timestamp} at<br><span class="tx">${state.transactionID}</span></p>
+<a href="" id="dl-canvas" download="${state.names && state.names[0]}_heart_${state.names && state.names[1]}.png">${!state.pending && 'Download your unique heart'}</a>
 `
 
 const main = (state) => html`${state.pending ? pending : confirmed(state)}`
