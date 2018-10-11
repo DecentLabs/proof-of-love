0e52c74cbc009331a462e9f222e262feabf85bc2
Daniel Fogaras <daniel.fogaras@gmail.com>
2018-10-11 11:01:30 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+# Add your aws user to the group proofoflove-admin
+# export AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+# Always run from the directory of the deploy script.
+aws s3 cp source/ 's3://proofoflove/' --recursive
