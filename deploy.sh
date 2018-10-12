1da9dee2f65c09bedf2c827a4e39cfeb722f5513
Daniel Fogaras <daniel.fogaras@gmail.com>
2018-10-12 14:27:05 +0200

    add build to deploy

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 # Add your aws user to the group proofoflove-admin
 # export AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
 # Always run from the directory of the deploy script.
-aws s3 cp public/ 's3://proofoflove/' --recursive
+yarn clean && yarn build && aws s3 cp public/ 's3://proofoflove/' --recursive
