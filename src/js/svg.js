ee75ba8c60ba5fa058f406666dcc12f988b92ef7
juli_e <julia.ero@euedge.com>
2018-11-06 15:04:42 +0100

    origami svg update

--- src/js/svg.js
+++ src/js/svg.js
@@ -16,14 +16,17 @@ export const getSvg = (imageUrl, names, qrCode) => {
 		<image overflow="visible" width="160" height="160" href="${imageUrl}" transform="matrix(0.8375 0 0 0.8375 233 69)"></image>
 		<text transform="matrix(1 0 0 1 159.457 210.3564)" font-size="16px">${name1}</text>
 		<text transform="matrix(1 0 0 1 387.5127 210.3564)" font-size="16px">${name2}</text>
-    <text transform="matrix(6.123234e-17 -1 1 6.123234e-17 91.0312 352.4873)" font-size="14px">Blockchain data</text>
-    <text transform="matrix(-5.551115e-17 1 -1 -5.551115e-17 508.9863 225.8616)" font-size="14px">stays forever</text>
-    <text transform="matrix(-1 0 0 -1 389.4863 410.957)" font-size="11px">https://proofoflove.digital</text>
-    <text transform="matrix(6.123234e-17 -1 1 6.123234e-17 122.418 319.1738)" font-size="9px">So does this love</text>
-    <text transform="matrix(6.123234e-17 1 -1 6.123234e-17 478.2148 227.8262)" font-size="9px">letter remain eternal</text>
-    <text transform="matrix(6.123234e-17 -1 1 6.123234e-17 149.3677 265.6323)" font-size="9px">See the</text>
-    <text transform="matrix(6.123234e-17 1 -1 6.123234e-17 451.2344 228.2332)" font-size="9px">message...</text>
-    <image style="overflow:visible;" width="320" height="320" href="${qrCode}"  transform="matrix(0.2812 0 0 0.2812 255 300)"></image>
+    <text transform="matrix(0 -1 1 0 91.0312 352.4873)" font-size="14px" >Blockchain data</text>
+    <text transform="matrix(0 1 -1 0 508.9863 227.8616)" font-size="14px" >stays forever</text>
+    <text transform="matrix(0 -1 1 0 122.418 319.1738)" font-size="9px">So does this love</text>
+    <text transform="matrix(0 1 -1 0 477.2148 227.8262)" font-size="9px">remain eternal</text>
+    <text transform="matrix(0 -1 1 0 151.3677 287.6318)" font-size="9px" >carved into</text>
+    <text transform="matrix(0 1 -1 0 448.2344 228.2332)" font-size="9px" >the blockchain</text>
+    <text transform="matrix(0 -1 1 0 181.3675 265.6318)" font-size="9px" >See the</text>
+    <text transform="matrix(0 1 -1 0 418.2324 228.2333)" font-size="9px" >message...</text>
+    <text transform="matrix(-1 0 0 -1 412.2324 410.957)" font-size="11px">all that is captured of this love:</text>
+    <text transform="matrix(-1 0 0 -1 371.4863 307.957)" font-size="9px">https://proofoflove.digital</text>
+    <image height="320" overflow="visible" transform="matrix(.25 0 0 .25 259.992 320.992)" width="320" href="${qrCode}"/>
 	</g>
 </svg>
 `
