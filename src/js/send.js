839e36e7635c7c2edd542ee46ba5fd893b1e3f4d
juli_e <julia.ero@euedge.com>
2018-11-07 13:24:22 +0100

    show if user not logged in metamask

--- src/js/send.js
+++ src/js/send.js
@@ -1,56 +1,66 @@
-import { getWeb3, prove, getNetwork } from './w3.js'
-import { createHeart, getColorPalette } from './heart.js'
+import { getWeb3, prove, getNetwork } from './w3.js';
+import { createHeart, getColorPalette } from './heart.js';
 
-const card = document.getElementById('card')
+const card = document.getElementById('card');
 
 document.addEventListener('click', (e) => {
-  const button = document.getElementById('flip-button')
+  const button = document.getElementById('flip-button');
   if (e.target === button) {
 
-    card.classList.add('flip')
+    card.classList.add('flip');
   }
-})
+});
 
 window.addEventListener('load', () => {
-  const loveForm = document.getElementById('love-form')
-  const name1Element = document.getElementById('name1')
-  const name2Element = document.getElementById('name2')
-  const homeCanvas = document.getElementById('home-canvas')
-  const hash = `0x${'f00f'.repeat(16)}`
-  const palette = getColorPalette(true)
+  const loveForm = document.getElementById('love-form');
+  const errormsg = document.getElementById('error-msg');
+  const name1Element = document.getElementById('name1');
+  const name2Element = document.getElementById('name2');
+  const homeCanvas = document.getElementById('home-canvas');
+  const hash = `0x${'f00f'.repeat(16)}`;
+  const palette = getColorPalette(true);
 
-  createHeart(hash, homeCanvas, palette)
+  createHeart(hash, homeCanvas, palette);
 
-  let ready = null
+  let ready = null;
 
   if (window.ethereum) {
-    getWeb3(ethereum)
-    ready = ethereum.enable()
+    getWeb3(ethereum);
+    ready = ethereum.enable();
   } else if (window.web3) {
     // Use Mist/MetaMask's provider
-    getWeb3(web3.currentProvider)
-    ready = Promise.resolve()
+    getWeb3(web3.currentProvider);
+    ready = Promise.resolve();
   } else {
-    ready = Promise.reject()
-    console.log('No web3? You should consider trying MetaMask!')
-    card.classList.add('no-metamask')
+    ready = Promise.reject();
+    console.log('No web3? You should consider trying MetaMask!');
+    card.classList.add('no-metamask');
   }
 
-  ready.then(() => loveForm.addEventListener('submit', function (event) {
-    event.preventDefault()
+  ready.then(() => {
+    web3.eth.getAccounts((err, acc) => {
+      if (acc.length === 0) {
+        console.log('no acc')
+        card.classList.add('no-login');
+      }
+    });
 
-    const name1 = name1Element.value
-    const name2 = name2Element.value
-    if (name1 && name2 && name1.trim().length && name2.trim().length) {
-      Promise.all([
-        prove(name1, name2),
-        getNetwork()
-      ]).then(
-        ([hash, network]) => {
-          if (hash !== undefined) {
-            window.location.href = `/proof.html?${hash}${ network !== '1' ? '@' + network : ''}`
-          }
-        })
-    }
-  }))
-})
+    loveForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      const name1 = name1Element.value;
+      const name2 = name2Element.value;
+      if (name1 && name2 && name1.trim().length && name2.trim().length) {
+        Promise.all([
+          prove(name1, name2),
+          getNetwork(),
+        ]).then(
+          ([hash, network]) => {
+            if (hash !== undefined) {
+              window.location.href = `/proof.html?${hash}${ network !== '1' ? '@' + network : ''}`;
+            }
+          });
+      }
+    });
+  });
+});
