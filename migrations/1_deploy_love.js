39f252a5b3ad78b346eb09d9a21822244fe11b06
Treer Zoltán <treer@euedge.com>
2019-07-04 16:32:22 +0200

    use test relay hub on ropsten

--- migrations/1_deploy_love.js
+++ migrations/1_deploy_love.js
@@ -1,5 +1,5 @@
 const ProofOfLove = artifacts.require("./ProofOfLove.sol");
-const RelayHubAddress = "";
+const RelayHubAddress = "0x1349584869A1C7b8dc8AE0e93D8c15F5BB3B4B87"; // ropsten gas relay hub
 
 module.exports = function(deployer) {
     deployer.then(async () => {
